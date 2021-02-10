7732a45e696a511d5689df655f0e01b48ff8f31b
Vlad Mykol <vlad.mykol@localhost>
2021-02-11 00:12:03 +0200

    add logging of all requests

--- src/main/java/com/vladmykol/takeandcharge/security/JwtAuthorizationFilter.java
+++ src/main/java/com/vladmykol/takeandcharge/security/JwtAuthorizationFilter.java
@@ -14,10 +14,24 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.Arrays;
 
 @Slf4j
 @Component
 public class JwtAuthorizationFilter extends BasicAuthenticationFilter {
+    private final String[] IP_HEADER_NAMES = {
+            "X-Forwarded-For",
+            "Proxy-Client-IP",
+            "WL-Proxy-Client-IP",
+            "HTTP_X_FORWARDED_FOR",
+            "HTTP_X_FORWARDED",
+            "HTTP_X_CLUSTER_CLIENT_IP",
+            "HTTP_CLIENT_IP",
+            "HTTP_FORWARDED_FOR",
+            "HTTP_FORWARDED",
+            "HTTP_VIA",
+            "REMOTE_ADDR"
+    };
     private final JwtProvider jwtProvider;
     private final CustomUserDetailsService customUserDetailsService;
 
@@ -35,9 +49,13 @@ public class JwtAuthorizationFilter extends BasicAuthenticationFilter {
 
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain chain) throws IOException, ServletException {
-        authenticate(request);
-
-        chain.doFilter(request, response);
+        String ipAddress = getRemoteIP(request);
+        try {
+            authenticate(request);
+            chain.doFilter(request, response);
+        } finally {
+            log.debug("Incoming request from {} to {} {} - {}", ipAddress, request.getMethod(), request.getRequestURL(), response.getStatus());
+        }
     }
 
     private void authenticate(HttpServletRequest request) {
@@ -59,6 +77,15 @@ public class JwtAuthorizationFilter extends BasicAuthenticationFilter {
         } else {
             SecurityContextHolder.clearContext();
         }
+    }
+
+    public String getRemoteIP(HttpServletRequest request) {
+        String ip = Arrays.stream(IP_HEADER_NAMES)
+                .map(request::getHeader)
+                .filter(h -> h != null && h.length() != 0 && !"unknown".equalsIgnoreCase(h))
+                .map(h -> h.split(",")[0])
+                .reduce("", (h1, h2) -> h1 + ":" + h2);
+        return ip + request.getRemoteAddr();
     }
 
 
