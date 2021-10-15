a53e5c4b45e72cbed60f63f1ce241f73846e22b6
Vlad Mykol <vlad.mykol@localhost>
2021-10-15 21:51:25 +0300

    add info about last login

--- src/main/java/com/vladmykol/takeandcharge/monitoring/StationConnectionMonitor.java
+++ src/main/java/com/vladmykol/takeandcharge/monitoring/StationConnectionMonitor.java
@@ -15,7 +15,7 @@ public class StationConnectionMonitor {
     private final TelegramNotifierService telegramNotifierService;
     private final StationRepository stationRepository;
 
-    @Scheduled(fixedRate = 6 * MILLIS_PER_MINUTE, initialDelay = 3 * MILLIS_PER_MINUTE)
+    @Scheduled(fixedRate = 13 * MILLIS_PER_MINUTE, initialDelay = 3 * MILLIS_PER_MINUTE)
     public void monitorClients() {
         final var disconnectedStations = stationRegister.getDisconnectedStations();
         if (!disconnectedStations.isEmpty()) {
