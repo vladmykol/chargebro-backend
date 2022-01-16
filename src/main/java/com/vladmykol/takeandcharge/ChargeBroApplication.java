7cbf1d357c80caf4bdb18d05c7436a3f7cb8d6fa
Vlad Mykol <vlad.mykol@localhost>
2022-01-16 20:32:37 +0200

    Revert "update to java 17"
    This reverts commit e7bd131f

--- src/main/java/com/vladmykol/takeandcharge/ChargeBroApplication.java
+++ src/main/java/com/vladmykol/takeandcharge/ChargeBroApplication.java
@@ -2,6 +2,8 @@ package com.vladmykol.takeandcharge;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.filter.CommonsRequestLoggingFilter;
 
 @SpringBootApplication
 public class ChargeBroApplication {
@@ -10,16 +12,16 @@ public class ChargeBroApplication {
         SpringApplication.run(ChargeBroApplication.class, args);
     }
 
-//    @Bean
-//    public CommonsRequestLoggingFilter logFilter() {
-//        CommonsRequestLoggingFilter filter
-//                = new CommonsRequestLoggingFilter();
-//        filter.setIncludeQueryString(true);
-//        filter.setIncludePayload(true);
-//        filter.setMaxPayloadLength(10000);
-//        filter.setIncludeHeaders(false);
-//        return filter;
-//    }
+    @Bean
+    public CommonsRequestLoggingFilter logFilter() {
+        CommonsRequestLoggingFilter filter
+                = new CommonsRequestLoggingFilter();
+        filter.setIncludeQueryString(true);
+        filter.setIncludePayload(true);
+        filter.setMaxPayloadLength(10000);
+        filter.setIncludeHeaders(false);
+        return filter;
+    }
 
 
 }
